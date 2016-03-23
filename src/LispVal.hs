7442327805b407f3f5e12bbb27ae20f0f8702d86
Stephen Diehl <stephen.m.diehl@gmail.com>
2016-03-23 00:16:20 -0400

    parsec parser using text

--- src/LispVal.hs
+++ src/LispVal.hs
@@ -1,1 +1,12 @@
 module LispVal where
+
+import qualified Data.Text as T
+
+data LispVal
+  = Atom T.Text
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String T.Text
+  | Bool Bool
+  deriving (Show)
--- src/Pretty.hs
+++ src/Pretty.hs
@@ -1,1 +1,18 @@
-module Pretty where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Pretty (
+) where
+
+import Text.PrettyPrint.Leijen.Text
+import Data.Text.Lazy (fromStrict)
+
+import LispVal
+
+instance Pretty LispVal where
+  pretty expr = case expr of
+    Atom str   -> text (fromStrict str)
+    Number i   -> pretty i
+    String s   -> dquotes (text (fromStrict s))
+    Bool True  -> text "#t"
+    Bool False -> text "#f"
+    List ls    -> encloseSep lparen rparen space (fmap pretty ls)

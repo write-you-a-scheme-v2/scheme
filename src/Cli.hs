19d9e6bb47f02685a8d3a582622ce9d8a6a29f51
Adam Wespiser <adamwespiser@gmail.com>
2016-11-07 03:56:04 +0000

    got basics to CLI working

--- src/Cli.hs
+++ src/Cli.hs
@@ -1,22 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Cli (
-  cliIface,
+  cliIfaceSimple,
+  cliIface
 ) where
 
+import Eval
 import System.IO
 import System.Environment --getArgs
-import Eval
 import Data.Text as T
 import Control.Monad.Trans
 import Options.Applicative
 import Options.Applicative
-import Data.Semigroup ((<>))
+--import Data.Semigroup ((<>))
 
 
 --http://book.realworldhaskell.org/read/io.html
 
-process :: String -> IO ()
-process = evalText $ T.pack str
+process :: T.Text -> IO ()
+process txt = evalText txt
 
 cliIface :: IO ()
 cliIface = do 
@@ -31,38 +32,37 @@ mainloop inh  =  do
   ineof <- hIsEOF inh
   if ineof
     then  putStr "empty file\n" >> return ()
-      else do fileText <- T.pack $ readFile $ inh 
-              process fileText
+      else do fileText <- hGetContents $ inh 
+              process $ T.pack fileText
 
 -- https://github.com/pcapriotti/optparse-applicative
 -- https://hackage.haskell.org/package/optparse-applicative
 
 data LineOpts = LineOpts
-  { script :: T.Text
+  { script :: String
   , useRepl :: Bool 
   }
 
 parseLineOpts :: Parser LineOpts
 parseLineOpts = LineOpts
     <$> strOption
-        ( long (T.unpack "script")
-       <> short "s"
+        ( long "script"
+       <> short 's'
        <> metavar "SCRIPT"
        <> value "" --default to empty string so we don't need to specify in the case of using the REPL
        <> help "File containing the script you want to run")
     <*> switch
         ( long "repl"
-       <> short "r"
+       <> short 'r'
        <> help "Run as interavtive read/evaluate/print/loop")
 
 schemeEntryPoint :: LineOpts -> IO ()
 schemeEntryPoint (LineOpts script False) = putStrLn $ "Run script: " ++ script -- script
 schemeEntryPoint (LineOpts "" True) = putStrLn $ "Run repl " --repl
-schemeEntryPoint (LineOpts script True) = putStrLn $ "Run script " 
-++ script ++ " then go into repl " --script, then repl
+schemeEntryPoint (LineOpts script True) = putStrLn $ "Run script " ++  script ++ " then go into repl " --script, then repl
 
-main :: IO ()
-main = execParser opts >>= schemeEntryPoint
+cliIface :: IO ()
+cliIface = execParser opts >>= schemeEntryPoint
   where
     opts = info (helper <*> parseLineOpts)
       ( fullDesc

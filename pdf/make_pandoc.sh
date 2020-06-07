19ed3e0a208743f5ae3ef4b266087ded60c36a9f
Adam Wespiser <adamwespiser@gmail.com>
2020-06-06 22:13:53 -0400

    Add pdf of all the docs

---
+++ pdf/make_pandoc.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Generate the docs individually using pandoc, keeping the headers, then stitch them together
+# This is less than ideal, since I couldn't find a great cross platform pdf library
+
+# THUS:
+# this script MUST be run on MacOS, on Linux you could do:
+# expression is:
+# pandoc -s -o WriteYouASchemeVersion2.pdf $(ls ../docs | sort -n)
+
+mkdir -p /tmp/scheme-pdf
+rm /tmp/scheme-pdf/*.pdf
+
+for markdown in $(ls ../docs | sort -n)
+do
+  echo $markdown
+  pandoc -s -o /tmp/scheme-pdf/"${markdown}_tail.pdf" ../docs/$markdown
+done
+
+"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o WriteYouASchemeVersion2.pdf $(ls /tmp/scheme-pdf/*.pdf)
